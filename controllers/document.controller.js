268bdfd046c6397deb0690f536978fd24f4863c2
Syakina <2111522017_syakina@student.unand.ac.id>
2023-06-26 04:14:29 +0700

    add landing page

--- controllers/document.controller.js
+++ controllers/document.controller.js
@@ -86,8 +86,7 @@ const updateSingleDocument = async (req, res) => {
 		const document = await Document.findOne({
 			where: { id: document_id, owner_id: id },
 		});
-
-		if (!document) return res.status(404).json({ message: 'Document not found' });
+		if (!document) return res.status(404).json({ error: 'Document not found' });
 
 		const data = {
 			title,
@@ -96,15 +95,16 @@ const updateSingleDocument = async (req, res) => {
 
 		if (req.file) {
 			unlinkSync(document.file);
+			console.log(req.file);
 			const { path } = req.file;
 			data.file = path.replace(/\\/g, '/');
 		}
 
 		await document.update(data);
-		return res.status(200).json(document);
+		return res.status(200).json({ message: 'Document updated successfully' });
 	} catch (error) {
 		console.error('Error updating single document:', error);
-		return res.status(500).json({ message: 'Internal server error' });
+		return res.status(500).json({ error: 'Internal server error' });
 	}
 };
 
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -4,20 +4,9 @@ const fs = require('fs');
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		switch (file.fieldname) {
-			case 'image':
-				cb(null, 'uploads/images');
-				break;
-			case 'file':
-				cb(null, 'uploads/documents');
-				break;
-			case 'signature':
-				cb(null, 'uploads/signatures');
-				break;
-			default:
-				cb(null, 'uploads/images');
-				break;
-		}
+		const dir = `uploads/${file.fieldname}`;
+		if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+		cb(null, dir);
 	},
 	filename: (req, file, cb) => {
 		const uniqueSuffix = Date.now();
@@ -25,10 +14,34 @@ const storage = multer.diskStorage({
 	},
 });
 
+const multerDoc = multer({
+	storage: storage,
+	limits: { fileSize: 5000000 },
+	fileFilter: (req, file, cb) => {
+		const filetypes = /pdf|doc|docx|xls|xlsx|ppt|pptx/;
+		const mimetype = filetypes.test(file.mimetype);
+		const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+		if (mimetype && extname) return cb(null, true);
+		cb('Error: File upload only supports the following filetypes - ' + filetypes);
+	},
+});
+
+const multerImage = multer({
+	storage: storage,
+	limits: { fileSize: 2000000 },
+	fileFilter: (req, file, cb) => {
+		const filetypes = /jpeg|jpg|png|gif/;
+		const mimetype = filetypes.test(file.mimetype);
+		const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+		if (mimetype && extname) return cb(null, true);
+		cb('Error: Image upload only supports the following filetypes - ' + filetypes);
+	},
+});
+
 module.exports = {
-	uploadImages: multer({ storage }).fields([
+	uploadImages: multerImage.fields([
 		{ name: 'image', maxCount: 1 },
 		{ name: 'signature', maxCount: 1 },
 	]),
-	uploadFile: multer({ storage }).single('file'),
+	uploadFile: multerDoc.single('file'),
 };
